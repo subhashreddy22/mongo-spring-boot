9042252330594cabb4533a01245d83f5e760229b
Subhash Reddy Duvvuru <subhashreddy@Subhashs-MacBook-Air.local>
2021-08-04 19:43:02 +0530

    add initial project

---
+++ src/main/groovy/com/learn/mongo/MongodbSpringBootApplication.groovy
@@ -0,0 +1,13 @@
+package com.learn.mongo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MongodbSpringBootApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MongodbSpringBootApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/learn/mongo/MongodbSpringBootApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.learn.mongo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongodbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
